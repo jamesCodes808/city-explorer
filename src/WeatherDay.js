cf188be88b636d4c44c92988aac4d559900a80e7
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-02-24 16:02:30 -0800

    renders timestamp of when API was called

--- src/WeatherDay.js
+++ src/WeatherDay.js
@@ -9,7 +9,10 @@ class WeatherDay extends React.Component {
                 {this.props.weatherInfo.map(day =>
                     <section>
                         <p>Day: {day.date}</p> <br></br>
+                        <p><img src={day.icon} alt={day.description}></img></p><br></br>
                         <p>Forecast: {day.description}</p><br></br>
+                        <p>Max Temp: {day.maxTemp}</p><br></br>
+                        <p>Min Temp: {day.minTemp}</p><br></br>
                     </section>
                 )}
 
